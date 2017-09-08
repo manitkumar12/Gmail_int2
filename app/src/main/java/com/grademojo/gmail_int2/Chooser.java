f4c9cb9278c46b0c582e14c938d28f26092a1af2
manit <ermanit007@gmail.com>
2017-09-08 17:48:07 +0530

    google integration

---
+++ app/src/main/java/com/grademojo/gmail_int2/Chooser.java
@@ -0,0 +1,14 @@
+package com.grademojo.gmail_int2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Chooser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chooser);
+    }
+}
